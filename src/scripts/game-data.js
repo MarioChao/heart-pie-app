adbffae2ba2ddec68402149f99b90ef8822ce7e4
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-10-26 12:15:52 +0800

    saved place ids update

--- src/scripts/game-data.js
+++ src/scripts/game-data.js
@@ -4,11 +4,14 @@ const savedGamePlaceIds = {
 	"Dream Game": 5475056496,
 	"The Forge and the Crucible": 6989453447,
 	"The Roulette Saloon": 6787210828,
-	"Juke's Tower of Hell": 8562822414,
 	"sun, moon, and stars. to these i bid adieu": 7554879061,
 	"Pie Hiking Difficulty Test": 16124983043,
 	"Dimensions (merak64)": 8927874968,
 	"Myth Game": 11656808939,
+	"POSTMORTEM": 16549530059,
+	"late july, midsummer": 14107732720,
+	"High Hike": 14834601229,
+	"Juke's Tower of Hell": 8562822414,
 };
 
 export { savedGamePlaceIds };
