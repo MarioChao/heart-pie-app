12dda0d5ad9c12b3e868afe9a1d592e25a511713
MarioChao <89794497+MarioChao@users.noreply.github.com>
2025-06-22 04:16:05 +0800

    game data: added new games & removed some for the 25 limit In the future, I may implement "slicing" such that autocmplete can work with more than 25 options

--- src/scripts/game-data.js
+++ src/scripts/game-data.js
@@ -1,23 +1,30 @@
 // Constants
 
 const savedGamePlaceIds = {
+	"Badge Points": 88622467292584,
+	"CRAZY HORSE GAME": 15716048273,
 	"Dimensions": 8927874968,
 	"Displacement and Destination": 9678077803,
 	"DOORS": 6516141723,
+	"Dream Dive": 18296261309,
 	"Dream Game": 5475056496,
+	"Dream Universe": 12931465480,
 	"DREAM WORLD": 3957129026,
 	"Eternal Tower of Hell": 8562822414,
-	"Gabs's Pie-Hiking Trials": 107460738144519,
 	"High Hike": 14834601229,
 	"Inver~o": 13621526714,
 	"late july, midsummer": 14107732720,
 	"Myth Game": 11656808939,
-	"Pie Hiking Difficulty Test": 16124983043,
+	"Nevermoor's Murder Mystery": 13779221881,
+	"Pie Hiking Badger": 94427741197530,
+	"Pool": 7198876449,
+	"Project Somnia": 14474926935,
 	"POSTMORTEM": 16549530059,
 	"something, somewhere ❄️": 10850058564,
 	"sun, moon, and stars. to these i bid adieu": 7554879061,
 	"The Forge and the Crucible": 6989453447,
 	"The Roulette Saloon": 6787210828,
+	"Ultra Hard Badge List: Reborn": 16859146415,
 	"Viper Enclave": 10734356776,
 };
 
